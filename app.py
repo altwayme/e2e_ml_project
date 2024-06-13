2f0ab8168f00d6f98519b7bfad5c9ad8ff263a91
Nikita <altwayme@gmail.com>
2024-06-13 06:48:12 +0000

    Working ML model, tested in Postman

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, request
+import joblib
+import numpy
+import sklearn
+
+MODEL_PATH = 'mlmodels/model.pkl'
+SCALER_X_PATH = 'mlmodels/scaler_x.pkl'
+SCALER_Y_PATH = 'mlmodels/scaler_y.pkl'
 
 app = Flask(__name__)
+model = joblib.load(MODEL_PATH)
+sc_x = joblib.load(SCALER_X_PATH)
+sc_y = joblib.load(SCALER_Y_PATH)
 
-@app.route("/admin")
-def hello_admin():
-    return "Hello Admin"
+@app.route('/predict_price', methods = ['GET'])
+def predict():
+    args = request.args
+    floor = args.get('floor', default = -1, type = int)
+    rooms = args.get('rooms', default = -1, type = int)
+    studio = args.get('studio', default = -1, type = bool)
+    area = args.get('area', default = -1, type = float)
+    renovation = args.get('renovation', default = -1, type = int)
 
 
-if __name__ == ""__main__":
-    app.run(debug=True)
-    
+    x = numpy.array([floor, rooms, studio, area, renovation]).reshape(1, -1)
+    x = sc_x.transform(x)
+
+    result = model.predict(x)
+    result = sc_y.inverse_transform(result.reshape(1,-1))
+
+    return str(result[0][0])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5444, host='0.0.0.0')
